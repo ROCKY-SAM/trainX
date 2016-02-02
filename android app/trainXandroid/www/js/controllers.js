eb1af89a6a670dc0ccccae8f94a1b1cc7f8c6a82
kavioshanaiesecer <kavi.oshan8@gmail.com>
2016-02-02 11:17:08 +0530

    reservation and payment successfull

--- android app/trainXandroid/www/js/controllers.js
+++ android app/trainXandroid/www/js/controllers.js
@@ -31,3 +31,12 @@ angular.module('app.controllers', [])
 .controller('reservationDetails', function($scope) {
 
 })
+
+
+.controller('paymentMethod', function($scope) {
+
+})
+
+.controller('paymentSuccess', function($scope) {
+
+})
